191c94a83f22bab637699a2c57cfb0df9877857c
Carlos Atkinson Ferreira Filho <carlos.atks@gmail.com>
2016-10-08 15:56:19 -0300

    Champions with images

--- models/champion.rb
+++ models/champion.rb
@@ -1,29 +1,51 @@
 require 'rest-client'
 require 'json'
 class Champion
-  API_CHAMPIONS = "https://global.api.pvp.net/api/lol/static-data/br/v1.2/champion?api_key=#{ENV['API_KEY']}"
-  attr_reader :id,:name,:title
+  API_CHAMPIONS = "https://global.api.pvp.net/api/lol/static-data/br/v1.2/champion?champData=image&api_key=#{ENV['API_KEY']}"
+  CHAMPION_IMAGE_CDN = "http://ddragon.leagueoflegends.com/cdn/6.19.1/img/champion/"
+  extend Enumerable
 
-  def initialize(id:,name:,title:)
+  attr_reader :id,:name,:title,:image
+
+  def initialize(id:,name:,title:,image:)
     @id = id
     @name = name
     @title = title
+    @image = image
   end
 
-  def self.all
-    response = fetch_champions
-    parser(response)
+  def image_url
+    CHAMPION_IMAGE_CDN + @image
   end
 
-  def self.fetch_champions
-    RestClient.get(API_CHAMPIONS)
-  end
+  #Class Methods
+  class << self
+    def all
+      parser(response)
+    end
 
-  private
+    def each(&block)
+      all.each(&block)
+    end
 
-  def self.parser(response)
-    JSON.parse(response.body)['data'].map do |k,v|
-      Champion.new(id: v['id'],name: v['name'],title: v['title'])
+    def sample
+      all.sample
+    end
+
+    def fetch_champions
+      @@response = RestClient.get(API_CHAMPIONS)
+    end
+
+    def response
+      @@response ||= fetch_champions
+    end
+
+    private
+    def parser(response)
+      JSON.parse(response.body)['data'].map do |k,v|
+        Champion.new(id: v['id'],name: v['name'],title: v['title'],image: v.dig('image','full'))
+      end
     end
   end
+
 end
--- spec/models/champion_spec.rb
+++ spec/models/champion_spec.rb
@@ -3,16 +3,25 @@ require_relative '../spec_helper.rb'
 describe Champion do
 
   let(:all_champions){Champion.all}
-  it "responds to the id,name and title" do
-    @champion = Champion.new(id:0,name:'Teste Testador',title:'O Grande Teste')
-    [:id,:name,:title].each do |attribute|
-      expect(@champion).to respond_to(attribute)
+  let(:champion_attributes){[:id,:name,:title,:image]}
+
+  it "has access to the API Key" do
+    expect(ENV['API_KEY']).not_to be_nil
+  end
+
+  it "has attributes" do
+    champion = Champion.sample
+    champion_attributes.each do |attribute|
+      expect(champion.send(attribute).to_s).to match(/.+/)
     end
   end
+
   it "fecth data from api" do
-    expect(Champion.fetch_champions.code).to eq(200)
+    expect(Champion.response.code).to eq(200)
   end
+
   it "return champions" do
     expect(all_champions).to be_a Enumerable
   end
+
 end
